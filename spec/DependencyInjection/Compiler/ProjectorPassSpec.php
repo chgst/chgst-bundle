5ffd8c12dae984abfcfb5fa55405cfe208501fa1
Janusz Slota <janusz.slota@nixilla.com>
2018-08-29 16:35:30 +0100

    adds listener and projector pass to register services to buses

--- spec/DependencyInjection/Compiler/ProjectorPassSpec.php
+++ spec/DependencyInjection/Compiler/ProjectorPassSpec.php
@@ -2,34 +2,34 @@
 
 namespace spec\Changeset\ChangesetBundle\DependencyInjection\Compiler;
 
-use Changeset\ChangesetBundle\DependencyInjection\Compiler\HandlerPass;
-use Changeset\Communication\CommandBusInterface;
+use Changeset\ChangesetBundle\DependencyInjection\Compiler\ProjectorPass;
+use Changeset\Communication\EventBusInterface;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
 
-class HandlerPassSpec extends ObjectBehavior
+class ProjectorPassSpec extends ObjectBehavior
 {
     function it_is_initializable()
     {
-        $this->shouldHaveType(HandlerPass::class);
+        $this->shouldHaveType(ProjectorPass::class);
         $this->shouldHaveType(CompilerPassInterface::class);
     }
 
-    function it_registers_all_services_tagged_as_handler_to_command_bus(ContainerBuilder $builder, Definition $definition)
+    function it_registers_all_services_tagged_as_projector_to_event_bus(ContainerBuilder $builder, Definition $definition)
     {
-        $builder->has(CommandBusInterface::class)->willReturn(false);
+        $builder->has(EventBusInterface::class)->willReturn(false);
         $this->process($builder);
 
 
-        $builder->has(CommandBusInterface::class)->willReturn(true);
+        $builder->has(EventBusInterface::class)->willReturn(true);
         $builder->findDefinition(Argument::any())->willReturn($definition);
 
         $services = [ 'someId' => [ ['event' => 'some.event' ]]];
 
-        $builder->findTaggedServiceIds('changeset.command.handler')->willReturn($services);
+        $builder->findTaggedServiceIds('changeset.event.projector')->willReturn($services);
         $definition->addMethodCall(Argument::any(), Argument::any())->shouldBeCalled();
         $this->process($builder);
     }
--- src/ChangesetBundle.php
+++ src/ChangesetBundle.php
@@ -3,6 +3,8 @@
 namespace Changeset\ChangesetBundle;
 
 use Changeset\ChangesetBundle\DependencyInjection\Compiler\HandlerPass;
+use Changeset\ChangesetBundle\DependencyInjection\Compiler\ListenerPass;
+use Changeset\ChangesetBundle\DependencyInjection\Compiler\ProjectorPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
@@ -11,5 +13,7 @@ class ChangesetBundle extends Bundle
     public function build(ContainerBuilder $container)
     {
         $container->addCompilerPass(new HandlerPass());
+        $container->addCompilerPass(new ListenerPass());
+        $container->addCompilerPass(new ProjectorPass());
     }
 }
